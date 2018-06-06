cbcaf3b427468fd1a097ef9e6a99b6e6b8d6f01c
sukumar <sukku183msd@gmail.com>
2018-06-06 16:37:22 +0530

    first server push

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require("./config/databse")
+
+//connect To Database
+mongoose.connect(config.database)
+
+//On Connection
+mongoose.connection.on('connected', () => {
+    console.log('conected to database '+ config.database)
+})
+
+//On Error
+mongoose.connection.on('err', () => {
+    console.log('Database error'+ err)
+})
+
+const app = express();
+
+const users = require('./routes/users')
+
+//Port Number
+const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+//Users Routes
+app.use('/users', users);
+//console.log(users)
+
+//Index Route
+app.get('/', (req, res) => {``
+    res.send('Invalid Endpoint.')
+    console.log('/ root index route.');
+})
+
+app.get('*', (req,res) => {
+    res.sendfile(path.join(__dirname, 'public/index.html'))
+})
+
+//Start Server
+app.listen(port, () => {
+    console.log('server started on Port ' + port)
+})
+
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+//const config = require("./config/databse")
+
+//User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function (id, cb) {
+    User.findById(id, cb)
+}
+
+module.exports.getUserByUsername = function (username, cb) {
+    console.log(username)
+    const query = { username: username }
+    User.findOne(query, cb)
+}
+
+module.exports.addUser = function (newUserObj, cb) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUserObj.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUserObj.password = hash;
+            newUserObj.save(cb)
+        })
+    })
+}
+
+module.exports.comparePassword = function (candidatePwd, hash, cb) {
+    bcrypt.compare(candidatePwd, hash, (err, isMatch) => {
+        if (err) throw err;
+        cb(null, isMatch)
+    })
+}
